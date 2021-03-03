e4d6de1231245a5d90636d4453c33e2e46e1aa6b
Yunyun <yunyun@pengyunyundeMBP.hitronhub.home>
2021-03-03 00:36:31 -0800

    comment input update

--- src/comps/CommentInput/index.js
+++ src/comps/CommentInput/index.js
@@ -7,46 +7,42 @@ const Container = styled.div`
     min-width: 414px;
     min-height: 71px;
     background-color: white;
-`;
-
-const Avatar = styled.img`
-    border-radius: 50%;
-    width: 50px;
-    height: 50px;
-    margin-right: 10px;
+    button {
+        min-width: 55px;
+        min-height: 40px;
+        border-radius: 16px;
+        border: none;
+        background-color: #54BAF3;
+        color: white;
+        box-sizing: border-box;
+        margin-left: 12px;
+        cursor: pointer;
+    }
 `;
 
 const Comment = styled.div`
     display: flex;
     align-items: center;
     input {
-        min-width: 316px;
-        min-height: 50px;
+        min-width: 300px;
+        min-height: 40px;
         border: none;
         box-sizing: border-box;
         box-shadow: 0px 0px 5px #C4C4C4;
-        border-radius: 25px;
+        border-radius: 16px;
     }
-    img {
-        width: 37px;
-        height: 37px;
-        margin-left: -45px;
-        cursor: pointer;
-    }  
 `;
 
-const CmtInput = ({imgurl, onClick}) => {
+const CmtInput = ({onClick}) => {
     return <Container>
-        <Avatar src={imgurl}/>
         <Comment>
             <input type="text" />
-            <img src="/icons/submit.svg" alt="submit" onClick={onClick}/>
         </Comment>
+        <button><h5>Send</h5></button>
     </Container>
 }
 
 CmtInput.defaultProps = {
-    imgurl:"/icons/default_profile.svg",
 };
 
 export default CmtInput;
