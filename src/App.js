df57491032924914e39463b59a7a7cc68ca40ce1
joshrenema2 <60946895+joshrenema2@users.noreply.github.com>
2021-03-03 15:29:44 -0800

    adding jwt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.scss';
+import react, { useEffect, useState } from 'react';
+import jwtDecode from "jwt-decode";
+import useLocalStorage from "react-use-localstorage";
 import Button from "comps/Button/default";
 import RoundButton from 'comps/Button/round';
 import ProfileInfo from 'comps/ProfileInfo';
@@ -9,6 +12,14 @@ import SinglePost from 'comps/SinglePost';
 import TopNav from 'comps/TopNav';
 
 function App() {
+  const [token, setToken] = useLocalStorage("token");
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const user = token ? jwtDecode(token) : null;
+    setUser(user);
+  }, [token]);
+
   return <div>
     <MultiLineInput />
   </div>
