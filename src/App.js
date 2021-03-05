524de1a93d984812519a190b0e27cf989b4a8841
joshrenema2 <60946895+joshrenema2@users.noreply.github.com>
2021-03-05 15:50:53 -0800

    push

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./App.scss";
 import axios from "axios";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+import useLocalStorage from "react-use-localstorage";
 
 import About from 'pages/about';
 import Login from 'pages/login';
