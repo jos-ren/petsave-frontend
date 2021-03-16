134199927be5b57c0fb61fc3e5ec503c03586d2b
cpark3918 <cindypark31@gmail.com>
2021-03-16 02:35:46 -0700

    login done

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -7,6 +7,31 @@ import Button from 'comps/Button/default';
 
 function Login ({}) {
     const history = useHistory();
+    const [username, setUsername] = useState("");
+    const [pwd, setPwd] = useState("");
+
+    const [error, setError] = useState(null);
+
+    const handleLogin = async () => {
+        const resp = await axios.post("https://petsave-backend.herokuapp.com/api/login",{
+            username: username,
+            pwd: pwd
+        });
+        console.log(resp);
+
+        if(resp.data !== "Unautorized" && resp.data !== "Incorrect credentials"){
+            const token = resp.data.token;
+            // 1) store the token in the storage
+            sessionStorage.setItem("token", token);
+            // 2) connect token with axios header
+            axios.defaults.headers.common['Authorization'] = token;
+            // 3) redirect to the main page
+            history.push("/home")
+        } else {
+            // update a state to show an error
+            setError("Incorrect email or password")
+        }
+    }
 
     return (
         <div className="page">
@@ -16,7 +41,9 @@ function Login ({}) {
                 <Input header="Password" type="password" placeholder="*********"/>
             </div>
             <div className="login_but">
-                <Button text="log in" bgcolor="#54BAF3"/>
+                <Button text="log in" bgcolor="#54BAF3"
+                onClick={handleLogin}
+                />
                 <div>
                     <p>Don't have an account?</p>
                     <Button text="register" disable={true}
