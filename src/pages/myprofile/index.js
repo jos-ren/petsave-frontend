959a8b6e2fe608a2dc50456711aca90c2e5b8cc7
Yunyun0729 <ufttysdyun0729@gmail.com>
2021-03-22 15:08:59 -0700

    TRY GET POST

--- src/pages/myprofile/index.js
+++ src/pages/myprofile/index.js
@@ -139,13 +139,13 @@ function UserProfile({ }) {
                     name={user.fullname}
                     imgurl={user.profile_pic}
                     bio={user.bio}
-                />        
-                <HomeFeed 
-                    img={posts.img_src} 
-                    id={posts.id}
-                    onPostClick={()=>{
-                            history.push("/post/"+params.id)
-                }}/>
+                />
+                
+                {posts.map((o, i) => 
+                <HomeFeed key={i} img={o.img_src} onPostClick={()=>{
+                    history.push("/post/"+o.id)
+                }}/>)}
+
                 <NavBar profileIcon='icons/profile.svg' />
 
             </div>
